c62a6fa3ac357a8c740bb40986b7131d16effa37
mattril3y <m@mjriley.net>
2016-09-29 20:27:40 +0100

    Added missing file

---
+++ Emailzing.UI/ViewModels/ContentViewModel.cs
@@ -0,0 +1,102 @@
+﻿using Emailzing.Model;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Emailzing.UI.ViewModels
+{
+	public class ContentViewModel : IContentViewModel, INotifyPropertyChanged
+	{
+		public ContentViewModel(
+			Email email)
+		{
+			From = email.From;
+			To = email.To;
+			Title = email.Title;
+			FullTime = email.FullTime;
+			Content = email.Content;
+		}
+
+		public void Update(Email email)
+		{
+			From = email.From;
+			To = email.To;
+			Title = email.Title;
+			FullTime = email.FullTime;
+			Content = email.Content;
+		}
+
+		private string _from;
+
+		public string From
+		{
+			get { return _from; }
+			set
+			{
+				_from = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+
+		private string _to;
+
+		public string To
+		{
+			get { return _to; }
+			set
+			{
+				_to = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+
+		private string _title;
+
+		public string Title
+		{
+			get { return _title; }
+			set
+			{
+				_title = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+
+		private string _fullTime;
+
+		public string FullTime
+		{
+			get { return _fullTime; }
+			set
+			{
+				_fullTime = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		private string _content;
+
+		public string Content
+		{
+			get { return _content; }
+			set
+			{
+				_content = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
